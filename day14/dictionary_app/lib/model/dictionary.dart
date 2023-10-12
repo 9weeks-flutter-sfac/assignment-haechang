218c73c261d4a6e0f5eb0686bdb316ca3f674eec
haechang <94176916+rohhaechang@users.noreply.github.com>
2023-10-12 17:01:50 +0900

    class 수정

--- day14/dictionary_app/lib/model/dictionary.dart
+++ day14/dictionary_app/lib/model/dictionary.dart
@@ -1,11 +1,12 @@
-class Dictionary {
-  final String word;
-  final String phonetic;
-  final List<Phonetics> phonetics;
-  final List<Meaning> meanings;
-  final License license;
-  final List<String> sourceUrls;
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 
+class Dictionary {
+  String word;
+  String phonetic;
+  List phonetics;
+  List<Meaning> meanings;
+  License license;
+  String sourceUrls;
   Dictionary({
     required this.word,
     required this.phonetic,
@@ -14,113 +15,83 @@ class Dictionary {
     required this.license,
     required this.sourceUrls,
   });
-
-  factory Dictionary.fromJson(Map<String, dynamic> json) {
-    final List<dynamic> phoneticsList = json['phonetics'];
-    final List<Phonetics> phonetics = phoneticsList
-        .map((e) => Phonetics.fromJson(e as Map<String, dynamic>))
-        .toList();
-
-    final List<dynamic> meaningsList = json['meanings'];
-    final List<Meaning> meanings = meaningsList
-        .map((e) => Meaning.fromJson(e as Map<String, dynamic>))
-        .toList();
-
+  factory Dictionary.fromMap(Map<String, dynamic> map) {
     return Dictionary(
-      word: json['word'] as String,
-      phonetic: json['phonetic'] as String,
-      phonetics: phonetics,
-      meanings: meanings,
-      license: License.fromJson(json['license'] as Map<String, dynamic>),
-      sourceUrls: List<String>.from(json['sourceUrls'] as List<dynamic>),
-    );
+        word: map['word'],
+        phonetic: map['phonetic'],
+        phonetics:
+            List<Phonetic>.from(map['phonetics']), // phonetics를 따로 만들지 않고 적용
+        meanings: List<Meaning>.from(map['meanings']),
+        license: map['license'],
+        sourceUrls: map['sourceUrls']);
   }
 }
 
-class Phonetics {
-  final String text;
-  final String audio;
-  final String sourceUrl;
-
-  Phonetics({
+class Phonetic {
+  String text;
+  String audio;
+  String sourceUrl;
+  Phonetic({
     required this.text,
     required this.audio,
     required this.sourceUrl,
   });
-
-  factory Phonetics.fromJson(Map<String, dynamic> json) {
-    return Phonetics(
-      text: json['text'] as String,
-      audio: json['audio'] as String,
-      sourceUrl: json['sourceUrl'] as String,
-    );
-  }
-}
-
-class Meaning {
-  final String partOfSpeech;
-  final List<Definition> definitions;
-  final List<String> synonyms;
-  final List<String> antonyms;
-  final String? example;
-
-  Meaning({
-    required this.partOfSpeech,
-    required this.definitions,
-    required this.synonyms,
-    required this.antonyms,
-    required this.example,
-  });
-
-  factory Meaning.fromJson(Map<String, dynamic> json) {
-    final List<dynamic> definitionsList = json['definitions'];
-    final List<Definition> definitions = definitionsList
-        .map((e) => Definition.fromJson(e as Map<String, dynamic>))
-        .toList();
-
-    return Meaning(
-      partOfSpeech: json['partOfSpeech'] as String,
-      definitions: definitions,
-      synonyms: List<String>.from(json['synonyms'] as List<dynamic>),
-      antonyms: List<String>.from(json['antonyms'] as List<dynamic>),
-      example: json['example'] != null ? json['example'] : null,
-    );
-  }
-}
-
-class Definition {
-  final String definition;
-  final List<String> synonyms;
-  final List<String> antonyms;
-
-  Definition({
-    required this.definition,
-    required this.synonyms,
-    required this.antonyms,
-  });
-
-  factory Definition.fromJson(Map<String, dynamic> json) {
-    return Definition(
-      definition: json['definition'] as String,
-      synonyms: List<String>.from(json['synonyms'] as List<dynamic>),
-      antonyms: List<String>.from(json['antonyms'] as List<dynamic>),
-    );
+  factory Phonetic.fromMap(Map<String, dynamic> map) {
+    return Phonetic(
+        text: map['text'], audio: map['audio'], sourceUrl: map['sourceUrl']);
   }
 }
 
 class License {
-  final String name;
-  final String url;
-
+  String name;
+  String url;
   License({
     required this.name,
     required this.url,
   });
 
-  factory License.fromJson(Map<String, dynamic> json) {
-    return License(
-      name: json['name'] as String,
-      url: json['url'] as String,
-    );
+  factory License.fromMap(Map<String, dynamic> map) {
+    return License(name: map['name'], url: map['url']);
+  }
+}
+
+class Meaning {
+  String partOfSpeech;
+  List<Definition> definitions;
+  List<String> synonyms;
+  List antonyms;
+  Meaning({
+    required this.partOfSpeech,
+    required this.definitions,
+    required this.synonyms,
+    required this.antonyms,
+  });
+  factory Meaning.fromMap(Map<String, dynamic> map) {
+    return Meaning(
+        partOfSpeech: map['partOfSpeech'],
+        definitions: List<Definition>.from(map['definitions']),
+        synonyms: map['synonyms'],
+        antonyms: map['antonyms']);
+  }
+}
+
+class Definition {
+  String definition;
+  List synonyms;
+  List antonyms;
+  String? example;
+  Definition({
+    required this.definition,
+    required this.synonyms,
+    required this.antonyms,
+    required this.example,
+  });
+
+  factory Definition.fromMap(Map<String, dynamic> map) {
+    return Definition(
+        definition: map['definition'],
+        synonyms: map['synonyms'],
+        antonyms: map['antonyms'],
+        example: map['example']);
   }
 }
